56f3d34bc03fd03c5f6fa93b2f62b7d319ceda72
Walter H. Palladino <wpalladino@gmail.com>
2023-04-24 20:50:35 -0300

    Added Cross Product to help check polygon convex

--- src/math/Point2dInt.h
+++ src/math/Point2dInt.h
@@ -18,4 +18,24 @@ public:
 		this->x = p.x;
 		this->y = p.y;
 	}
+
+	Point2dInt operator-(const Point2dInt &p) const
+	{
+		Point2dInt diff;
+
+		diff.x = x - p.x;
+		diff.y = y - p.y;
+
+		return diff;
+	}
+
+	int Cross(Point2dInt p1, Point2dInt p2, Point2dInt p3)
+	{
+		//	Get the vectors
+		Point2dInt v1 = p2 - p1;
+		Point2dInt v2 = p3 - p1;
+
+		// Return cross product
+		return (v1.x * v2.y - v1.y * v2.x);
+	}
 };
