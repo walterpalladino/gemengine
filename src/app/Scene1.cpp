5079375fd7f0c7ecdeaa9873d86fc58eeb3c0d55
Walter H. Palladino <wpalladino@gmail.com>
2024-04-10 08:08:16 -0300

    Code cleaning

--- src/app/Scene1.cpp
+++ src/app/Scene1.cpp
@@ -2,19 +2,19 @@
 
 #include "app/Scene1.h"
 
-#include "core/graphics/image/Image.h"
-#include "core/graphics/sprites/Sprite.h"
-#include "core/graphics/text/Text.h"
 #include "utils/StringUtils.h"
 #include "utils/Log.h"
+
 #include "core/input/InputHandler.h"
 #include "core/graphics/text/FontsManager.h"
 #include "core/graphics/console/Console.h"
 #include "core/graphics/draw2d/Draw.h"
 #include "core/sound/SoundManager.h"
 #include "core/scenes/Scene.h"
+
 #include "core/scenes/SceneManager.h"
 #include "core/renderer/RenderManager.h"
+
 #include "core/Context.h"
 #include "core/GemEngine.h"
 #include "core/events/EventManager.h"
@@ -76,7 +76,6 @@ SceneTransition Scene1::Loop(float time)
 {
 
     SceneTransition sceneTransition;
-    // cout << "Scene1::Loop" << endl;
 
     //   Scale the image
     Vector3d scale = ledArrow->transform.scale;
@@ -89,17 +88,14 @@ SceneTransition Scene1::Loop(float time)
 
     if (InputHandler::Instance()->IsKeyDown(SDL_SCANCODE_A))
     {
-        // std::cout << "SDL_SCANCODE_A" << std::endl;
         rotationSpeed = -5.0f;
     }
     else if (InputHandler::Instance()->IsKeyDown(SDL_SCANCODE_D))
     {
-        // std::cout << "SDL_SCANCODE_D" << std::endl;
         rotationSpeed = 5.0f;
     }
-    // rotationSpeed = 10.0f;
+
     Vector3d rotation = ledArrow->transform.rotation;
-    // rotation.z = rotationSpeed * time / 1000.0f;
     rotation.z += rotationSpeed;
 
     if (rotation.z > 360)
@@ -125,80 +121,49 @@ SceneTransition Scene1::Loop(float time)
     fpsText->SetText(text_info);
     delete[] text_info;
 
-    // cout << "App::Loop Console operation here..." << endl;
     RenderManager::Instance()->backgroundColor = Point3dInt(216, 216, 216);
     RenderManager::Instance()->backgroundColor = Point3dInt(80, 69, 155);
     Console::Instance()->SetColor(0, 0, 0);
     Console::Instance()->SetColor(136, 126, 203);
 
-    //    Console::Instance()->SetCursorAt(0, 0);
-    //    Console::Instance()->Print("GemEngine running...");
-    //    Console::Instance()->SetCursorAt(25, 0);
-    //    Console::Instance()->Print("still running...");
-    // Console::Instance()->Print(text);
-
     if (InputHandler::Instance()->WasKeyReleased(SDL_SCANCODE_1))
     {
-        // cout << "Play Music Track" << endl;
-        // SoundManager::Instance()->PlayTrack("SlingerSwaggerLoop");
         track->Play();
     }
     if (InputHandler::Instance()->WasKeyReleased(SDL_SCANCODE_2))
     {
-        // cout << "Stop Music Track" << endl;
-        // SoundManager::Instance()->StopTrack();
         track->Stop();
     }
 
     if (InputHandler::Instance()->WasKeyReleased(SDL_SCANCODE_3))
     {
-        // sound_channel = SoundManager::Instance()->PlaySound("ScussesSound1");
-        //  cout << "Playing Sound : " << sound_channel << endl;
         sound->Play();
     }
     if (InputHandler::Instance()->WasKeyReleased(SDL_SCANCODE_4))
     {
-        // cout << "Stop Sound : " << sound_channel << endl;
-        // SoundManager::Instance()->StopSound(sound_channel);
         sound->Stop();
     }
 
     if (InputHandler::Instance()->WasKeyReleased(SDL_SCANCODE_7))
     {
-        // cout << "Reduce Music Track Volume" << endl;
-        // int musicVolume = SoundManager::Instance()->SetTrackVolume(-1);
         int musicVolume = track->SetVolume(-1);
-        // cout << "musicVolume = " << musicVolume << endl;
-        // musicVolume = SoundManager::Instance()->SetTrackVolume(musicVolume - 1);
         musicVolume = track->SetVolume(musicVolume - 1);
-        // cout << "musicVolume = " << musicVolume << endl;
     }
     if (InputHandler::Instance()->WasKeyReleased(SDL_SCANCODE_8))
     {
-        // cout << "Increase Music Track Volume" << endl;
-        // int musicVolume = SoundManager::Instance()->SetTrackVolume(-1);
         int musicVolume = track->SetVolume(-1);
-        // cout << "musicVolume = " << musicVolume << endl;
-        // musicVolume = SoundManager::Instance()->SetTrackVolume(musicVolume + 1);
         musicVolume = track->SetVolume(musicVolume + 1);
-        // cout << "musicVolume = " << musicVolume << endl;
     }
 
     if (InputHandler::Instance()->WasKeyReleased(SDL_SCANCODE_9))
     {
-        // cout << "Reduce Master Channels Volume" << endl;
         int masterVolume = SoundManager::Instance()->SetMasterVolume(-1);
-        // cout << "masterVolume = " << masterVolume << endl;
         masterVolume = SoundManager::Instance()->SetMasterVolume(masterVolume - 1);
-        // cout << "masterVolume = " << masterVolume << endl;
     }
     if (InputHandler::Instance()->WasKeyReleased(SDL_SCANCODE_0))
     {
-        // cout << "Increase Master Channels Volume" << endl;
         int masterVolume = SoundManager::Instance()->SetMasterVolume(-1);
-        // cout << "masterVolume = " << masterVolume << endl;
         masterVolume = SoundManager::Instance()->SetMasterVolume(masterVolume + 1);
-        // cout << "masterVolume = " << masterVolume << endl;
     }
 
     //  Sprite Animations
@@ -243,7 +208,6 @@ SceneTransition Scene1::Loop(float time)
     {
         //  Next Scene
         string newSceneName = "Second Scene";
-        // cout << "Next Scene : " << newSceneName << endl;
         sceneTransition.scene = SceneManager::Instance()->GetScene(newSceneName);
         return sceneTransition;
     }
@@ -251,7 +215,6 @@ SceneTransition Scene1::Loop(float time)
     if (InputHandler::Instance()->WasKeyPressed(SDL_SCANCODE_P))
     {
         cout << "Pause Scene" << endl;
-        // sceneTransition.isPaused = true;
         SDL_Event event;
         SDL_memset(&event, 0, sizeof(event)); // or SDL_zero(event)
         event.type = EventManager::Instance()->GEMENGINE_EVENT_TYPE;
@@ -301,7 +264,6 @@ SceneTransition Scene1::Loop(float time)
 
 void Scene1::Clean()
 {
-    // cout << "Scene1::Clean" << endl;
 }
 
 void Scene1::OnMouseButtonDown(GemObject *object, SDL_Event *event)
